c54c8cd316a33a3f92f74e806d8a6d6b860e5e82
OgunColak <123779740+OgunColak@users.noreply.github.com>
2023-01-30 18:07:02 -0800

    Update FirstGitHubClass.java

--- src/Day1GitHub/FirstGitHubClass.java
+++ src/Day1GitHub/FirstGitHubClass.java
@@ -2,7 +2,7 @@ package Day1GitHub;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hey Github");
+        System.out.println("Hello Github");
         System.out.println(1+1);
     }
 }
