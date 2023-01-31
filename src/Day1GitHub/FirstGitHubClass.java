5c8460b5ecc824ef2441bae4668ec4e27f606deb
OgunColak <123779740+OgunColak@users.noreply.github.com>
2023-01-30 18:08:07 -0800

    Update FirstGitHubClass.java
    hey changed to helloo.

--- src/Day1GitHub/FirstGitHubClass.java
+++ src/Day1GitHub/FirstGitHubClass.java
@@ -2,7 +2,7 @@ package Day1GitHub;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hello Github");
+        System.out.println("Helloo Github");
         System.out.println(1+1);
     }
 }
