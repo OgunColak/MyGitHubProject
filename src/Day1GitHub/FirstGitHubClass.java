6e79b6a2999fc378cc07bef7dfde6527c3bb1e95
colak <colakogun@yahoo.com>
2023-01-30 17:25:34 -0800

    I added some changes for Math operation.

---
+++ src/Day1GitHub/FirstGitHubClass.java
@@ -0,0 +1,9 @@
+package Day1GitHub;
+
+public class FirstGitHubClass {
+    public static void main(String[] args) {
+        System.out.println("Hey Github");
+        System.out.println(1+1);
+    }
+}
+
